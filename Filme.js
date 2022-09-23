b7776042e4564bdeed967033d0fd29d1594da58b
Matheus Oliveira <matheusfotografia987@gmail.com>
2022-09-23 18:37:26 -0300

    Cria filme!

---
+++ Filme.js
@@ -0,0 +1,8 @@
+module.exports = Filme = (id, titulo, duracao, atores, anoDeLancamento, emCartaz) => {
+  this.id = id;
+  this.titulo = titulo;
+  this.duracao = duracao;
+  this.atores = atores;
+  this.anoDeLancamento = anoDeLancamento;
+  this.emCartaz = emCartaz;
+};
